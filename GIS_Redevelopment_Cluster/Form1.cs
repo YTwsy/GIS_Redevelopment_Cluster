1adfff2b7182ea4f7390eabf654c8a6428704e3d
YTwsy <94418335+YTwsy@users.noreply.github.com>
2021-11-16 10:32:53 +0800

    Update README.md

--- GIS_Redevelopment_Cluster/Form1.cs
+++ GIS_Redevelopment_Cluster/Form1.cs
@@ -55,6 +55,8 @@ namespace GIS_Redevelopment_Cluster
 
         public void initControls()
         {
+
+
             //MapControl控件在Panel2里
             this.splitContainer1.Panel2.Controls.Add(mapCtrl);
             mapCtrl.Width = this.splitContainer1.Panel2.Width;
@@ -165,17 +167,11 @@ namespace GIS_Redevelopment_Cluster
                 {
                     switch (type)
                     {
-                        //case GeometryType.VarLine:
-                        //    {
-                        //        _GeoVarLine = new GeoVarLine();
-                        //        _GeoVarLine = _Geometry as GeoVarLine;
-                        //    }
                         case GeometryType.Points:
                             {
                                 _Geopoints = new GeoPoints();
                                 _Geopoints = _Geometry as GeoPoints;
 
-                                //Dot3D dot = _Geopoints.GetItem(oid);
                                 Dots3D dots = _Geopoints.GetDots();
                                 Dot3D dot = dots.GetItem(0);
                                 double dot_x = dot.X;
@@ -212,6 +208,7 @@ namespace GIS_Redevelopment_Cluster
         {
             //string strTest = "该例子测试一个字符串写入文本文件。";
             //System.IO.File.WriteAllText(@"D:\test1.txt", strTest, Encoding.UTF8);
+
             Process p = new Process();
             //设置要启动的应用程序
             p.StartInfo.FileName = "K-means_PRO.exe";
@@ -235,8 +232,97 @@ namespace GIS_Redevelopment_Cluster
 
         }
 
-        public void Buffer(SFeatureCls resource_sfcls, DataBase GDB, string name)
+        ///// <summary>
+        ///// 打开CSV 文件
+        ///// </summary>
+        ///// <param name="fileName">文件全名</param>
+        ///// <param name="firstRow">开始行</param>
+        ///// <param name="firstColumn">开始列</param>
+        ///// <param name="getRows">获取多少行</param>
+        ///// <param name="getColumns">获取多少列</param>
+        ///// <param name="haveTitleRow">是有标题行</param>
+        ///// <returns>DataTable</returns>
+        
+        //public static DataTable OpenCSV(string fullFileName, Int16 firstRow = 0, Int16 firstColumn = 0, Int16 getRows = 0, Int16 getColumns = 0, bool haveTitleRow = true)
+        //{
+        //    DataTable dt = new DataTable();
+        //    FileStream fs = new FileStream(fullFileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+        //    StreamReader sr = new StreamReader(fs, System.Text.Encoding.Default);
+        //    //记录每次读取的一行记录
+        //    string strLine = "";
+        //    //记录每行记录中的各字段内容
+        //    string[] aryLine;
+        //    //标示列数
+        //    int columnCount = 0;
+        //    //是否已建立了表的字段
+        //    bool bCreateTableColumns = false;
+        //    //第几行
+        //    int iRow = 1;
+
+        //    //去除无用行
+        //    if (firstRow > 0)
+        //    {
+        //        for (int i = 1; i < firstRow; i++)
+        //        {
+        //            sr.ReadLine();
+        //        }
+        //    }
+
+        //    // { ",", ".", "!", "?", ";", ":", " " };
+        //    string[] separators = { "," };
+        //    //逐行读取CSV中的数据
+        //    while ((strLine = sr.ReadLine()) != null)
+        //    {
+        //        strLine = strLine.Trim();
+        //        aryLine = strLine.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+
+        //        if (bCreateTableColumns == false)
+        //        {
+        //            bCreateTableColumns = true;
+        //            columnCount = aryLine.Length;
+        //            //创建列
+        //            for (int i = firstColumn; i < (getColumns == 0 ? columnCount : firstColumn + getColumns); i++)
+        //            {
+        //                DataColumn dc
+        //                    = new DataColumn(haveTitleRow == true ? aryLine[i] : "COL" + i.ToString());
+        //                dt.Columns.Add(dc);
+        //            }
+
+        //            bCreateTableColumns = true;
+
+        //            if (haveTitleRow == true)
+        //            {
+        //                continue;
+        //            }
+        //        }
+
+
+        //        DataRow dr = dt.NewRow();
+        //        for (int j = firstColumn; j < (getColumns == 0 ? columnCount : firstColumn + getColumns); j++)
+        //        {
+        //            dr[j - firstColumn] = aryLine[j];
+        //        }
+        //        dt.Rows.Add(dr);
+
+        //        iRow = iRow + 1;
+        //        if (getRows > 0)
+        //        {
+        //            if (iRow > getRows)
+        //            {
+        //                break;
+        //            }
+        //        }
+
+        //    }
+
+        //    sr.Close();
+        //    fs.Close();
+        //    return dt;
+        //}
+        public void show_cluster_result(DataBase GDB)
         {
+            //        public void show_cluster_result(SFeatureCls resource_sfcls, DataBase GDB, string name)
+
             QueryDef _QueryDef = null;
             Rect rect = new Rect();
             RecordSet _RecordSet = null;
@@ -245,14 +331,165 @@ namespace GIS_Redevelopment_Cluster
             GeoVarLine _GeoVarLine = null;
             GeoLines _GeoLines = null;
             GeoPoints _Geopoints = null;
+            IGeometry point_Geometry = null;
+            RegInfo _RegInfo = null; //线的图形信息
+            PntInfo _pntInfo = null; //点的图形信息
+            SFeatureCls pntCls = null;
+            long oid = 0;
 
- 
+            //int id = 0;
+            ////创建点简单要素类
+            //SFeatureCls result_sfcls = null;
+            //result_sfcls = new SFeatureCls(GDB);
+            //id = result_sfcls.Create(name, GeomType.Reg, 0, 0, null);
 
+            //_Geopoints = new GeoPoints();
+            //_Geopoints = _Geometry as GeoPoints;
+
+            //point_Geometry = null;
+            //Dots3D dots;
+            //Dot3D dot;
+            ////point_Geometry = _Geopoints.SetDots(dots);
+            ////point_Geometry =_Geopoints.Append(dot);
+            //_Geopoints.SetDots(dots);
+
+            //result_sfcls.Append(point_Geometry, _RecordSet.Att, _pntInfo);
+
+            //string sqlTxt = string.Format("SELECT * FROM [GrapInfoDB].[dbo].[Detail_Table_2] WHERE Name = '{0}' AND Type = '{1}'", name, type);
+            //SqlCommand maxCmd = new SqlCommand(sqlTxt, sconn);
+            //SqlDataReader dr = maxCmd.ExecuteReader();
+
+            //while (dr.Read())
+            //{
+            //    #region 点
+            //    if (type == "点")
+            //    {
+
+            using (StreamReader sr = new StreamReader("D:/GIS/GIS二次开发/Debug_dev_0/k - means_point.txt"))
+            {
+                string line;
+                PntInfo pntInfo = null;     //点图形参数信息
+                pntInfo = new PntInfo();
+                int id = 0;
+                //创建面简单要素类
+                pntCls = new SFeatureCls(GDB);
+                id = pntCls.Create("k-means_result", GeomType.Pnt, 0, 0, null);
+
+                // 从文件读取并显示行，直到文件的末尾 
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    string[] destStr = line.Split(',');
+                    //foreach (string outstr in destStr)
+                    //{
+                    //    Console.Write(outstr);
+                    //    Console.Write(" ");
+                    //}
+                    Console.WriteLine(destStr[0]);
+                    Console.WriteLine(destStr[1]);
+
+                    PntInfo info = new PntInfo();
+                    _RegInfo = new RegInfo();
+                    //设置面的图形信息
+                    info.BackClr = 376;//绿色
+
+
+                    Dot dot = new Dot(double.Parse(destStr[1]), double.Parse(destStr[2]));
+                    Dot3D dot3D = new Dot3D(double.Parse(destStr[1]), double.Parse(destStr[1]), 0);
+
+                    QueryDef qf = new QueryDef();
+                    qf.SetNear(dot,5, 5);
+
+                    GeoPoints point = new GeoPoints();
+                    point.Append(dot3D);
+                    Record rcd = new Record();
+                    rcd.Fields = pntCls.Fields;
+                    pntCls.Append(point, rcd, pntInfo);
+
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    ////string spatialInfos = dr[3].ToString();
+                    ////string grapInfos = dr[4].ToString();
+
+                    ////string[] loc = spatialInfos.Split(',');
+                    ////Dot dot = new Dot(double.Parse(loc[0]), double.Parse(loc[1]));
+
+                    //PntInfo info = new PntInfo();
+                    //_RegInfo = new RegInfo();
+                    ////设置面的图形信息
+                    //info.BackClr = 376;//绿色
+                    ////info.SymID = int.Parse(grapInfos.Split(',')[0]);
+                    ////int i = int.Parse(grapInfos.Split(',')[1]);
+                    ////info.OutClr = new int[] { i, i, i };
+                    ////info.Height = float.Parse(grapInfos.Split(',')[2]);
+                    ////info.Width = float.Parse(grapInfos.Split(',')[3]);
+
+                    ////Dot3D dot3D = new Dot3D(dot.X, dot.Y, 0);
+                    //Dot3D dot3D = new Dot3D(double.Parse(destStr[1]), double.Parse(destStr[1]), 0);
+
+                    //GeoPoints point = new GeoPoints();
+                    //point.Append(dot3D);
+                    //Record rcd = new Record();
+                    //rcd.Fields = pntCls.Fields;
+                    //oid=pntCls.Append(point, rcd, info);
+
+                    //rcd.SetValue("ID", oid);
+                    //pntCls.UpdateAtt(oid, rcd);
+                    ////}/*end of if (type == "点")*/
+                    ////#endregion
+
+                    ////break;
+                }
+
+
+            }
+
+
+
+            //判断地图视图中是否有处于显示状态中的地图
+            if (this.mapCtrl.ActiveMap == null)
+            {
+                MessageBox.Show("请先在地图视图中显示一幅地图！！！");
+                return;
+            }
+
+            this._Tree.WorkSpace.BeginUpdateTree();
+            this._Tree.WorkSpace.BeginUpdateTree();
+            //附加矢量图层
+            VectorLayer vecLayer = new VectorLayer(VectorLayerType.SFclsLayer);
+            bool v = vecLayer.AttachData(pntCls);
+            //将图层添加到地图中
+            vecLayer.Name = pntCls.ClsName;
+            //获取激活地图
+            Map activeMap = this.mapCtrl.ActiveMap;
+            activeMap.Append(vecLayer);
+            vecLayer.DetachData();
+            //复位
+            this.mapCtrl.ActiveMap = activeMap;
+            this.mapCtrl.Restore();
+            this._Tree.WorkSpace.EndUpdateTree();
 
         }
         private void button2_Click(object sender, EventArgs e)
         {
-
+            show_cluster_result(GDB);
         }
     }
 }
